3e0d843c9e38b0f05d0b046c48a549479045353f
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-11-17 18:05:02 -0300

    Adding cli library

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ezeoleaf/tweet-random-repo
 
 go 1.14
+
+require (
+	github.com/urfave/cli v1.22.5
+	github.com/urfave/cli/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Tweet Random Repo",
+		Usage: "Tweet random repositories",
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
