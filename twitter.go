7bb9632ebbe0db656ce28ae294b1792775690f25
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-11-18 10:59:15 -0300

    Simplify tweet

--- twitter.go
+++ twitter.go
@@ -9,8 +9,11 @@ import (
 
 func tweetRepo(cfg Config, repo github.Repository) {
 
-	hashtags := "\n#" + *repo.Language + " #github"
-	toTweet := *repo.FullName + ": " + *repo.Description + "\n⭐️: " + strconv.Itoa(*repo.StargazersCount) + hashtags + "\n " + *repo.HTMLURL
+	hashtags := "#" + *repo.Language + " #github" + "\n"
+	title := *repo.FullName + ": " + *repo.Description + "\n"
+	stargazers := "⭐️: " + strconv.Itoa(*repo.StargazersCount) + "\n"
+
+	toTweet := title + stargazers + hashtags + *repo.HTMLURL
 
 	client := cfg.AccessCfg.GetTwitterClient()
 	_, _, err := client.Statuses.Update(toTweet, nil)
