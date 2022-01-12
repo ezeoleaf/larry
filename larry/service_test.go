19dc19c26153022ffaac06c0be27979e8f2dfa08
Ezequiel Olea Figueroa <ezequiel.olea.figueroa@schibsted.com>
2022-01-12 17:22:57 +0100

    add tests in service

--- larry/service_test.go
+++ larry/service_test.go
@@ -1,64 +1,73 @@
 package larry
 
-// func TestRun(t *testing.T) {
-// 	for _, tc := range []struct {
-// 		Name           string
-// 		mockProvider   mock.ProviderMock
-// 		mockPublishers map[string]Publisher
-// 		returnValue    error
-// 	}{
-// 		{
-// 			Name: "Test error on get content",
-// 			mockProvider: mock.ProviderMock{
-// 				GetContentToPublishFn: func() (string, error) {
-// 					return "", errors.New("some error")
-// 				},
-// 			},
-// 			mockPublishers: map[string]Publisher{
-// 				"mock": mock.PublisherMock{},
-// 			},
-// 			returnValue: errors.New("some error"),
-// 		},
-// 		{
-// 			Name: "Test get content and publish",
-// 			mockProvider: mock.ProviderMock{
-// 				GetContentToPublishFn: func() (string, error) {
-// 					return "content", nil
-// 				},
-// 			},
-// 			mockPublishers: map[string]Publisher{
-// 				"mock": mock.PublisherMock{
-// 					PublishContentFn: func(content string) (bool, error) {
-// 						return true, nil
-// 					},
-// 				},
-// 			},
-// 			returnValue: nil,
-// 		},
-// 	} {
-// 		t.Run(tc.Name, func(t *testing.T) {
-// 			s := Service{
-// 				Provider:   tc.mockProvider,
-// 				Publishers: tc.mockPublishers,
-// 			}
+import (
+	"errors"
+	"testing"
 
-// 			resp := s.Run()
+	"github.com/ezeoleaf/larry/domain"
+	"github.com/ezeoleaf/larry/mock"
+)
 
-// 			if tc.returnValue != nil {
-// 				if resp == nil {
-// 					t.Errorf("expected resp but got %v instead", resp)
-// 					return
-// 				}
+func TestRun(t *testing.T) {
+	for _, tc := range []struct {
+		Name           string
+		mockProvider   mock.ProviderMock
+		mockPublishers map[string]Publisher
+		returnValue    error
+	}{
+		{
+			Name: "Test error on get content",
+			mockProvider: mock.ProviderMock{
+				GetContentToPublishFn: func() (*domain.Content, error) {
+					return nil, errors.New("some error")
+				},
+			},
+			mockPublishers: map[string]Publisher{
+				"mock": mock.PublisherMock{},
+			},
+			returnValue: errors.New("some error"),
+		},
+		{
+			Name: "Test get content and publish",
+			mockProvider: mock.ProviderMock{
+				GetContentToPublishFn: func() (*domain.Content, error) {
+					content := domain.Content{}
+					return &content, nil
+				},
+			},
+			mockPublishers: map[string]Publisher{
+				"mock": mock.PublisherMock{
+					PublishContentFn: func(content *domain.Content) (bool, error) {
+						return true, nil
+					},
+				},
+			},
+			returnValue: nil,
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			s := Service{
+				Provider:   tc.mockProvider,
+				Publishers: tc.mockPublishers,
+			}
 
-// 				if tc.returnValue.Error() != resp.Error() {
-// 					t.Errorf("expected %v as value, got %v instead", tc.returnValue.Error(), resp.Error())
-// 					return
-// 				}
-// 			} else if tc.returnValue == nil && resp != nil {
-// 				t.Errorf("expected no resp but got %v instead", resp)
-// 			}
+			resp := s.Run()
 
-// 		})
-// 	}
+			if tc.returnValue != nil {
+				if resp == nil {
+					t.Errorf("expected resp but got %v instead", resp)
+					return
+				}
 
-// }
+				if tc.returnValue.Error() != resp.Error() {
+					t.Errorf("expected %v as value, got %v instead", tc.returnValue.Error(), resp.Error())
+					return
+				}
+			} else if tc.returnValue == nil && resp != nil {
+				t.Errorf("expected no resp but got %v instead", resp)
+			}
+
+		})
+	}
+
+}
--- mock/publisher.go
+++ mock/publisher.go
@@ -1,12 +1,14 @@
 package mock
 
+import "github.com/ezeoleaf/larry/domain"
+
 // PublisherMock is a mock Publisher
 type PublisherMock struct {
-	PublishContentFn func(content string) (bool, error)
+	PublishContentFn func(content *domain.Content) (bool, error)
 }
 
 // PublishContent calls the PublishContentFn
-func (p PublisherMock) PublishContent(content string) (bool, error) {
+func (p PublisherMock) PublishContent(content *domain.Content) (bool, error) {
 	if p.PublishContentFn == nil {
 		return false, nil
 	}
