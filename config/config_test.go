2e107773d35bc9c028af4d757fb0c7cc4951634a
Ezequiel Olea Figueroa <ezequiel.olea.figueroa@schibsted.com>
2022-07-08 14:28:38 +0200

    add more tests

--- config/config_test.go
+++ config/config_test.go
@@ -24,3 +24,42 @@ func TestGetNoHashtags(t *testing.T) {
 		t.Errorf("Expected 0, got %v", len(hs))
 	}
 }
+
+func TestGetCacheKeyPrefix(t *testing.T) {
+	for _, tc := range []struct {
+		Name           string
+		MockConfig     Config
+		ExpectedPrefix string
+	}{
+		{
+			Name:           "Should return empty",
+			MockConfig:     Config{},
+			ExpectedPrefix: "",
+		},
+		{
+			Name:           "Should return only topic",
+			MockConfig:     Config{Topic: "topic"},
+			ExpectedPrefix: "topic-",
+		},
+		{
+			Name:           "Should return only language",
+			MockConfig:     Config{Language: "lang"},
+			ExpectedPrefix: "lang-",
+		},
+		{
+			Name:           "Should return topic and language",
+			MockConfig:     Config{Topic: "topic", Language: "lang"},
+			ExpectedPrefix: "topic-lang-",
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			prefix := tc.MockConfig.GetCacheKeyPrefix()
+
+			if prefix != tc.ExpectedPrefix {
+				t.Errorf("expected %s, got %s", tc.ExpectedPrefix, prefix)
+			}
+		})
+
+	}
+
+}
