a80c92e5dc7235106e92ac2a29f6438257d4ccd3
Ezequiel Olea Figueroa <ezequiel.olea.figueroa@schibsted.com>
2022-12-08 16:41:49 +0100

    remove unused data

--- cmd/larry/main.go
+++ cmd/larry/main.go
@@ -24,7 +24,7 @@ import (
 var (
 	redisAddress = envString("REDIS_ADDRESS", "localhost:6379")
 
-	githubAccessToken      = envString("GITHUB_ACCESS_TOKEN", "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")
+	githubAccessToken      = envString("GITHUB_ACCESS_TOKEN", "")
 	githubPublishRepoOwner = envString("GITHUB_PUBLISH_REPO_OWNER", "")
 	githubPublishRepoName  = envString("GITHUB_PUBLISH_REPO_NAME", "")
 	githubPublishRepoFile  = envString("GITHUB_PUBLISH_REPO_FILE", "README.md")
